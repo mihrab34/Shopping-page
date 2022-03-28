f30d9e8b67183265e70517d11be374147d932977
Akintola Ramota <akintolarahmah@gmail.com>
2022-03-28 16:33:38 +0000

    Added and displayed wallet and cart items

--- components/shopping.js
+++ components/shopping.js
@@ -1,22 +1,80 @@
-function Shop(props) {
-  console.log(props);
-  const{title, description, price, image} = props.product
+function Product(props) {
+  const { title, price, image } = props.product;
   return (
-    <div className="col-md-3">
-      <div className="card p-5">
-        <img src={image} className="card-img-top" alt="Mens Clothing" />
-        <div className="card-body">
-          <h5 className="card-title">{title}</h5>
-          <h5 className="card-price">
-            <span>Price: </span>
-            ${price}
-          </h5>
-          <button href="#" className="btn btn-primary mt-3">
-            Add To Cart
-          </button>
+      <div className="col-lg-4 col-md-6 col-sm-12">
+        <div className="card p-5">
+          <img src={image} className="card-img-top" alt={title} />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <h5 className="card-price">
+              <span>Price: </span>${price}
+            </h5>
+            <button
+              href="#"
+              className="btn btn-primary mt-3"
+              onClick={() => props.onAddToCart(props.product)}>
+              Add To Cart
+            </button>
+          </div>
         </div>
       </div>
+  );
+}
+
+function Shop(props) {
+  const { products, onAddToCart } = props;
+  const shop = products.map((product) => (
+    <Product key={product.id} product={product} onAddToCart={onAddToCart} />
+  ));
+  return (
+    <div className="col-8">
+      <div className="row">{shop}</div>
     </div>
+  );
+}
+
+function Wallet(props) {
+  const { balance = 0, owner = {} } = props.wallet;
+  const { name, phoneNumber, email, address } = owner;
+  return (
+    <>
+      <div id="wallet">
+        <h1>Wallet</h1>
+        <div>
+          <h5>Name: {name}</h5>
+          <p>Wallet Balance: {balance}</p>
+          <p>phone Number: {phoneNumber}</p>
+          <p>Email: {email}</p>
+          <p>Address: {address}</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function CartItem(props) {
+  const{item} = props;
+  return (
+    <div>
+          <div>{item.title}</div>
+          <button onClick={() => onAddToCart(item)}>+</button>
+          <div>
+            {item.cqty} * ${item.price}
+          </div>
+        </div>
+  )
+}
+
+function Cart(props) {
+  const { cart } = props;
+  return (
+    <aside>
+      <h2>Cart Items</h2>
+      <div>{cart.length === 0 && <div>Cart is empty</div>}</div>
+      {cart.map((item) => (
+        <CartItem key={item.id} item={item} />
+      ))}
+    </aside>
   );
 }
 class Shopping extends React.Component {
@@ -25,6 +83,8 @@ class Shopping extends React.Component {
     this.state = {
       products: [],
       wallet: {},
+      cart: [],
+      // setCartItem: [],
     };
   }
 
@@ -73,15 +133,36 @@ class Shopping extends React.Component {
     }
   }
 
+  handleAddToCart = (product) => {
+    if(product.qty >= 1) {
+      const exist = this.state.cart.find((item) => item.id === product.id);
+      if (exist) {
+        alert(" Product already in Cart");
+      }else {
+        let cartItem = { ...product, ...{ cqty: 1 }};
+        let cart = [...this.state.cart, cartItem];
+        this.setState({
+        cart: cart
+      })
+      }
+    }
+  };
+
   render() {
-    const shop = this.state.products.map((product) => (
-      <Shop key={product.id} product={product} />
-    ));
     return (
       <>
-        <h1 className="text-center">Shop Our Products</h1>
-        <div className="container">
-          <div className="row">{shop}</div>
+        <div className="container-fluid">
+          <h1 className="text-center">Shop Our Products</h1>
+          <div className="row">
+            <Shop
+              products={this.state.products}
+              onAddToCart={this.handleAddToCart}
+            />
+            <div className="col-4">
+              <Wallet wallet={this.state.wallet} />
+              <Cart cart={this.state.cart} />
+            </div>
+          </div>
         </div>
       </>
     );
